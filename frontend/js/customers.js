d6353122f17464715f6d22c9208bedc542ba0b0a
PrNotGood <pierre.folgarait@gmail.com>
2022-05-19 23:08:52 +0200

    Modifica grafica html

--- frontend/js/customers.js
+++ frontend/js/customers.js
@@ -3,17 +3,25 @@ const app = express();*/
 
 
 $(document).ready(function(){
-    /*app.get('/customers', function(req, res){
-        customerData = JSON.parse(res);
-        //res.send('Hello World!');
-    });*/
 
-    var items = [];
+    /*fetch('../api/v1/customers')
+    .then((resp) => resp.json())
+    .then(function(data){
+      console.log(data);
+      //loggedUser.token = data.token;
+      //loggedUser.email = data.email;
+      //loggedUser.id = data.id;
+      //loggedUser.self = data.self;
+    })
+    .catch(error => console.error(error));*/
 
     $.getJSON( "./example.json", function(data) {
         $.each(data, function(i, f) {
           var tblRow = "<tr><td>" + f.id + "</td><td>" + f.nome + "</td><td>" + f.cognome + "</td><td>" + f.codiceFiscale + "</td></tr>";
           $(tblRow).appendTo("#customer-table tbody");
         });
+
+        $('#user-count').html('<p>Numero di Utenti:' + data.length + '</p>'); //fare lo stesso con le macchine
       });
+
 })
