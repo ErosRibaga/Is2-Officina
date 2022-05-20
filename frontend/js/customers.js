4882343676cb310f50ed7e7329b94e8ed3fb45f4
PrNotGood <pierre.folgarait@gmail.com>
2022-05-20 18:13:29 +0200

    Aggiunta pagine modifica cliente e rimuovi cliente (solo html)

--- frontend/js/customers.js
+++ frontend/js/customers.js
@@ -1,9 +1,32 @@
 /*const express = require('express');
 const app = express();*/
 
-var selecteditem;
+var selecteditem = { id: -1 };
 
 $(document).ready(function(){
+  
+  document.getElementById("addClient").onclick = function () {
+    location.href = "/frontend/add-customer.html"; //add link to addPage
+  };
+
+  document.getElementById("remClient").onclick = function () {
+    if(selecteditem != -1){
+      //chiamare api per la rimozione dal DB 
+      selecteditem = -1;
+    }
+  };
+
+  document.getElementById("modClient").onclick = function () {
+    if(selecteditem != -1){
+      fetch("/frontend/mod-customer.html", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(selecteditem)
+      }).then(res => {
+        console.log("Request complete! response:", res);
+      });
+    }
+  };
 
   fetch('http://localhost:8080/api/v1/operations/')
   .then((resp) => resp.json()) // Transform the data into json
@@ -32,7 +55,8 @@ $(document).ready(function(){
 });
 
 $(document).on('click','.clickable',function() {
-  selecteditem = $(this).html();
-  var splitted = selecteditem.split('<td>');
+  var splitted = $(this).html().split('<td>');
+  selecteditem.id = splitted[1].indexOf('<');
   console.log(splitted[1].substr(0, splitted[1].indexOf('<')));
+  //non la migliore delle soluzioni ma finchè visualizziamo l'id in questo modo è OK
 });
