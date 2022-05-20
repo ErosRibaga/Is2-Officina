0f1c52db6cf40813f8289164bea57377e6067e3e
PrNotGood <pierre.folgarait@gmail.com>
2022-05-20 11:54:05 +0200

    Aggiunto schema lettura dati da API e seleziona riga html

--- frontend/js/customers.js
+++ frontend/js/customers.js
@@ -1,27 +1,38 @@
 /*const express = require('express');
 const app = express();*/
 
+var selecteditem;
 
 $(document).ready(function(){
 
-    /*fetch('../api/v1/customers')
-    .then((resp) => resp.json())
-    .then(function(data){
-      console.log(data);
-      //loggedUser.token = data.token;
-      //loggedUser.email = data.email;
-      //loggedUser.id = data.id;
-      //loggedUser.self = data.self;
-    })
-    .catch(error => console.error(error));*/
+  fetch('http://localhost:8080/api/v1/operations/')
+  .then((resp) => resp.json()) // Transform the data into json
+  .then((data) => {
+    return data.map((operation) => {
+      var obj = {
+        title: operation.title,
+        self: operation.self,
+        employee: operation.employee,
+        car: operation.car,
+      };
+      //console.log(operation);
+    });
+  })
+  .catch((error) => console.error(error)); // If there is any error you will catch them here
 
     $.getJSON( "./example.json", function(data) {
         $.each(data, function(i, f) {
-          var tblRow = "<tr><td>" + f.id + "</td><td>" + f.nome + "</td><td>" + f.cognome + "</td><td>" + f.codiceFiscale + "</td></tr>";
+          var tblRow = "<tr class='clickable'><td>" + f.id + "</td><td>" + f.nome + "</td><td>" + f.cognome + "</td><td>" + f.codiceFiscale + "</td></tr>";
           $(tblRow).appendTo("#customer-table tbody");
         });
 
         $('#user-count').html('<p>Numero di Utenti:' + data.length + '</p>'); //fare lo stesso con le macchine
       });
 
-})
+});
+
+$(document).on('click','.clickable',function() {
+  selecteditem = $(this).html();
+  var splitted = selecteditem.split('<td>');
+  console.log(splitted[1].substr(0, splitted[1].indexOf('<')));
+});
