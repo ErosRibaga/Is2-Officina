d4bfec781033926b88754414d96e48302b49ccb8
PrNotGood <pierre.folgarait@gmail.com>
2022-05-23 10:27:06 +0200

    Cambiata visualizzazione lista, implementato metodo DELETE (non funzionante)

--- app/customers.js
+++ app/customers.js
@@ -2,39 +2,65 @@ const express = require('express');
 const router = express.Router();
 const Customer = require('./models/customer');
 
+//insert put before GET
+
 router.post('', async (req, res) => {
-    let customer = new Customer({
-      nome: req.body.nome,
-      cognome: req.body.cognome,
-      codiceFiscale: req.body.codiceFiscale,
-    });
-  
-    customer = await customer.save();
-  
-    let customerId = customer.id;
-  
-    res
-      .location('/api/v1/customers/' + customerId)
-      .status(201)
-      .send();
+  let customer = new Customer({
+    nome: req.body.nome,
+    cognome: req.body.cognome,
+    codiceFiscale: req.body.codiceFiscale,
   });
 
-  router.get('/', async (req, res) => {
-    
-    let customers = await Customer.find({});
+  customer = await customer.save();
+
+  let customerId = customer.id;
+
+  res
+    .location('/api/v1/customers/' + customerId)
+    .status(201)
+    .send();
+});
+
+router.get('/', async (req, res) => {
   
-    console.log(req.body);
-  
-    customers = customers.map((customer) => {
-      return {
-        self: '/api/v1/customer/' + customer.id,
-        nome: customer.nome,
-        cognome: customer.cognome,
-        codiceFiscale: customer.codiceFiscale,
-      };
-    });
-    res.status(200).json(customers);
+  let customers = await Customer.find({});
+
+  console.log(req.body);
+
+  customers = customers.map((customer) => {
+    return {
+      self: '/api/v1/customer/' + customer.id,
+      nome: customer.nome,
+      cognome: customer.cognome,
+      codiceFiscale: customer.codiceFiscale,
+    };
   });
+  res.status(200).json(customers);
+});
+
+
+//get a customer by the Id
+router.get('/:id', async (req, res) => {
+  let customer = await Customer.findById(req.params.id);
+  res.status(200).json({
+    self: '/api/v1/operations/' + operation.id,
+    nome: customer.nome,
+    cognome: customer.cognome,
+    nome: customer.nome,
+  });
+});
+
+router.delete('/:id', async (req, res) => {
+  let customer = await Customer.findById(req.params.id).exec();
+  if (!customer) {
+      res.status(404).send();
+      console.log('book not found');
+      return;
+  }
+  await customer.deleteOne();
+  console.log('customer removed');
+  res.status(204).send();
+});
 
   /*
   router.get('/customer/:customer', async (req, res) => {
--- frontend/js/customers.js
+++ frontend/js/customers.js
@@ -1,7 +1,32 @@
 /*const express = require('express');
 const app = express();*/
 
-var selecteditem = { id: -1 };
+function loadCustomers(){
+
+  fetch('http://localhost:8080/api/v1/customers/')
+  .then((resp) => resp.json()) // Transform the data into json
+  .then((data) => {
+    return data.map((customer) => {
+      var obj = {
+        id: customer.self,
+        nome: customer.nome,
+        cognome: customer.cognome,
+        codiceFiscale: customer.codiceFiscale,
+      };
+
+      var tblRow = "<tr class='clickable'><td>  <p hidden>" + obj.id.substring(obj.id.lastIndexOf('/')+1) + "</p>" + obj.nome + "</td><td>" + obj.cognome + "</td><td>" + obj.codiceFiscale + "</td></tr>";
+      $(tblRow).appendTo("#customer-table tbody");
+
+      $('#user-count').html('<p>Nume  ro di Utenti:' + data.length + '</p>'); //fare lo stesso con le macchine
+
+    });
+  })
+  .catch((error) => console.error(error)); // If there is any error you will catch them here
+}
+
+
+
+var selecteditemid = -1 ;
 
 $(document).ready(function(){
   
@@ -10,53 +35,50 @@ $(document).ready(function(){
   };
 
   document.getElementById("remClient").onclick = function () {
-    if(selecteditem != -1){
+    if(selecteditemid != -1){
       //chiamare api per la rimozione dal DB 
-      selecteditem = -1;
+
+      fetch("http://127.0.0.1:8080/api/v1/customers/", {
+        method: "DELETE",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(selecteditemid)
+      }).then(res => {
+        console.log("Request complete! response:", res);
+      });
+
+      selecteditemid = -1;
     }
   };
 
   document.getElementById("modClient").onclick = function () {
-    if(selecteditem != -1){
+    if(selecteditemid != -1){
       fetch("/frontend/mod-customer.html", {
         method: "POST",
         headers: {'Content-Type': 'application/json'}, 
-        body: JSON.stringify(selecteditem)
+        body: JSON.stringify(selecteditemid)
       }).then(res => {
         console.log("Request complete! response:", res);
       });
     }
   };
 
-  fetch('http://localhost:8080/api/v1/operations/')
-  .then((resp) => resp.json()) // Transform the data into json
-  .then((data) => {
-    return data.map((operation) => {
-      var obj = {
-        title: operation.title,
-        self: operation.self,
-        employee: operation.employee,
-        car: operation.car,
-      };
-      //console.log(operation);
-    });
-  })
-  .catch((error) => console.error(error)); // If there is any error you will catch them here
-
-    $.getJSON( "./example.json", function(data) {
-        $.each(data, function(i, f) {
-          var tblRow = "<tr class='clickable'><td>" + f.id + "</td><td>" + f.nome + "</td><td>" + f.cognome + "</td><td>" + f.codiceFiscale + "</td></tr>";
-          $(tblRow).appendTo("#customer-table tbody");
-        });
-
-        $('#user-count').html('<p>Numero di Utenti:' + data.length + '</p>'); //fare lo stesso con le macchine
-      });
+  loadCustomers();
 
 });
 
 $(document).on('click','.clickable',function() {
-  var splitted = $(this).html().split('<td>');
-  selecteditem.id = splitted[1].indexOf('<');
-  console.log(splitted[1].substr(0, splitted[1].indexOf('<')));
+  var elem = $(this);
+
+  $(".selected").removeClass("selected");
+  elem.addClass("selected");
+
+  var splitted = elem.html().split('<');
+  selecteditemid = splitted[2].substring(splitted[2].indexOf('>')+1)
+
+  console.log(selecteditemid);
+
+  //selecteditem.id = splitted[1].indexOf('<');
+  //console.log(splitted[1].substr(0, splitted[1].indexOf('<')));
   //non la migliore delle soluzioni ma finchè visualizziamo l'id in questo modo è OK
+  //da fixare appena integro il prendere l'id con API
 });
