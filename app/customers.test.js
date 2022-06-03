af308d7a2a00f78696cc9db58c29a27f2bdf2e41
ErosRibaga <erosribaga@hotmail.it>
2022-06-03 18:26:18 +0200

    Implemented test with MongoMemoryServer instead of using spyOn and mockImplementation

--- app/customers.test.js
+++ app/customers.test.js
@@ -3,11 +3,9 @@ const app = require('./app');
 const Customer = require('./models/customer');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-var customerSpyFindById;
-var customerSpy;
-
-// create a valid for an admin user
+// create a token for an admin user
 const adminToken = jwt.sign(
   {
     email: 'email',
@@ -18,7 +16,7 @@ const adminToken = jwt.sign(
   { expiresIn: 86400 }
 );
 
-// create a valid for a normal user
+// create a token for a normal user
 const userToken = jwt.sign(
   {
     email: 'email',
@@ -29,49 +27,28 @@ const userToken = jwt.sign(
   { expiresIn: 86400 }
 );
 
-describe('GET /api/v1/customers', () => {
-  beforeAll(async () => {
-    console.log(adminToken);
-    connection = await mongoose.connect(
-      'mongodb+srv://db_prova1:admin@cluster0.ijsod.mongodb.net/officina?retryWrites=true&w=majority',
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
+var mongod;
 
-    customerSpy = jest
-      .spyOn(Customer, 'find')
-      .mockImplementation((criterias) => {
-        return [
-          {            
-            id: 1010,
-            name: 'Mario',
-            surname: 'Draghi',
-            phone: '1234567890',
-          },
-          {
-            id: 1011,
-            name: 'Dario',
-            surname: 'Maghi',
-            phone: '1234567891',
-          },
-        ];
-      });
-    customerSpyFindById = jest
-      .spyOn(Customer, 'findById')
-      .mockImplementation((id) => {
-        if (id == 1010)
-          return {
-            id: 1010,
-            name: 'Mario',
-            surname: 'Draghi',
-            phone: '1234567890'
-          };
-        else return {};
-      });
+describe('GET /api/v1/customers', () => {
+  var customerId;
+
+  beforeAll(async () => {
+    // This will create an new instance of "MongoMemoryServer" and automatically start it
+    mongod = await MongoMemoryServer.create();
+    app.locals.db = await mongoose.connect(mongod.getUri());
+
+    var customer = new Customer({
+      name: 'Dario',
+      surname: 'Maghi',
+      phone: '1234567899',
+    });
+
+    customerId = await customer.save();
+    customerId = customerId._id;
   });
 
-  afterAll(() => {
-    customerSpyFindById.mockRestore();
-    mongoose.connection.close();
+  afterAll(async () => {
+    await mongoose.connection.close();
   });
 
   test('GET /api/v1/customers with admin user should respond with an array of customers', async () => {
@@ -81,30 +58,24 @@ describe('GET /api/v1/customers', () => {
       .expect('Content-Type', /json/)
       .expect(200, [
         {
-          self: '/api/v1/customers/1010',
-          name: 'Mario',
-          surname: 'Draghi',
-          phone: '1234567890',
-        },
-        {
-          self: '/api/v1/customers/1011',
+          self: '/api/v1/customers/' + customerId,
           name: 'Dario',
           surname: 'Maghi',
-          phone: '1234567891',
+          phone: '1234567899',
         },
       ]);
   });
 
   test('GET /api/v1/customers/:id with admin user should respond with an array of customers', async () => {
     return request(app)
-      .get('/api/v1/customers/1010')
+      .get('/api/v1/customers/' + customerId)
       .set('x-access-token', adminToken)
       .expect('Content-Type', /json/)
       .expect(200, {
-        self: '/api/v1/customers/1010',
-        name: 'Mario',
-        surname: 'Draghi',
-        phone: '1234567890',
+        self: '/api/v1/customers/' + customerId,
+        name: 'Dario',
+        surname: 'Maghi',
+        phone: '1234567899',
       });
   });
 
@@ -118,12 +89,22 @@ describe('GET /api/v1/customers', () => {
 });
 
 describe('POST /api/v1/customers', () => {
-  test('POST /api/v1/customers with name not specified', () => {
+  beforeAll(async () => {
+    // This will create an new instance of "MongoMemoryServer" and automatically start it
+    app.locals.db = await mongoose.connect(mongod.getUri());
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+    await mongod.stop();
+  });
+
+  test('POST /api/v1/customers with name not specified', async () => {
     return request(app)
       .post('/api/v1/customers')
       .set('x-access-token', adminToken)
       .set('Accept', 'application/json')
-      .expect(400, { error: 'Name not specified' });
+      .expect(400, { error: 'Some fields are empty or undefined' });
   });
 
   test('POST /api/v1/customers with surname not specified', () => {
@@ -132,7 +113,7 @@ describe('POST /api/v1/customers', () => {
       .set('x-access-token', adminToken)
       .set('Accept', 'application/json')
       .send({ name: 'name' })
-      .expect(400, { error: 'Surname not specified' });
+      .expect(400, { error: 'Some fields are empty or undefined' });
   });
 
   test('POST /api/v1/customers with phone not specified', () => {
@@ -141,16 +122,28 @@ describe('POST /api/v1/customers', () => {
       .set('x-access-token', adminToken)
       .set('Accept', 'application/json')
       .send({ name: 'name', surname: 'surname' })
-      .expect(400, { error: 'Phone not specified' });
+      .expect(400, { error: 'Some fields are empty or undefined' });
   });
 
-  test('POST /api/v1/customers with phone duplicate', () => {
+  test('POST /api/v1/customers with phone duplicate', async () => {
+    await request(app)
+      .post('/api/v1/customers')
+      .set('x-access-token', adminToken)
+      .send({ name: 'name1', surname: 'surname1', phone: '1234567890' });
     return request(app)
       .post('/api/v1/customers')
       .set('x-access-token', adminToken)
       .set('Accept', 'application/json')
-      .send({ name: 'name', surname: 'surname', phone: '1234567890' })
+      .send({ name: 'name2', surname: 'surname2', phone: '1234567890' })
       .expect(409, { error: 'Phone duplicate' });
   });
-});
 
+  test('POST /api/v1/customers with valid data', () => {
+    return request(app)
+      .post('/api/v1/customers')
+      .set('x-access-token', adminToken)
+      .set('Accept', 'application/json')
+      .send({ name: 'name', surname: 'surname', phone: '1324567899' })
+      .expect(201);
+  });
+});
