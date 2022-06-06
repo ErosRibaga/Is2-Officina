4d3b388a5357e5019c848185a7da1b71c79ee14c
PrNotGood <pierre.folgarait@gmail.com>
2022-06-06 12:14:51 +0200

    fixed login

--- frontend/js/accountManagement.js
+++ frontend/js/accountManagement.js
@@ -1,4 +1,4 @@
-function login(){
+function login() {
 
     var email = document.getElementById("textmail").value;
     var password = document.getElementById("textpassword").value;
@@ -6,22 +6,34 @@ function login(){
     fetch('http://localhost:8080/api/v1/authentications', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( { email: email, password: password } ),
+        body: JSON.stringify({ email: email, password: password }),
     })
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) { // Here you get the data to modify as you please
 
-        var loggedUser = {};
-        loggedUser.token = data.token;
-        loggedUser.email = data.email;
-        loggedUser.id = data.id;
+            if (data.success) {
+                var loggedUser = {};
+                loggedUser.token = data.token;
+                loggedUser.email = data.email;
+                loggedUser.id = data.id;
 
-        //Create cookies
-        document.cookie = "token=" + data.token;
-        document.cookie = "admin=" + data.admin;
+                //Create cookies
+                document.cookie = "token=" + data.token;
+                document.cookie = "admin=" + data.admin;
+                if (data.token != undefined || data.admin != undefined) {
+                    redirect('/frontend/agenda.html');
+                }
+            } else {
+                $('#error').text(data.message);
+            }
+            console.log(data);
+            return;
+        })
+        .catch(error => {
+            /*console.error(error);
+            throw error;*/
+        });
 
-        redirect('/frontend/agenda.html');
-        return;
-    })
-    .catch( error => console.error(error) );
+
+
 }
